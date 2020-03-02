11e5df31e2d815485e137aa5e2be773311f33544
lipengze <13619845120@163.com>
2020-03-03 01:55:25 +0800

    进一步添加中文注释

---
+++ lipengze-learn/src/main/java/com/coding/beanFactoryPostProcessor/TestBeanFactoryProcessor.java
@@ -0,0 +1,24 @@
+package com.coding.beanFactoryPostProcessor;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.AnnotatedBeanDefinition;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Classname TestBeanFactoryProcessor
+ * @Description
+ * @Date 2020/3/2 19:32
+ * @Created by 亢丁
+ *
+ * 从这里我们看出来，BeanFactoryPostProcessor 最主要的就是可以得到 beanFactory ，然后从bean工厂中得到我们想要的bd，然后可以给这个bd改变他的属性，比如说是从单例变为多例
+ */
+@Component
+public class TestBeanFactoryProcessor implements BeanFactoryPostProcessor {
+	@Override
+	public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+		AnnotatedBeanDefinition annotatedBeanDefinition = (AnnotatedBeanDefinition) beanFactory.getBeanDefinition("myDaoImpl");
+		annotatedBeanDefinition.setScope("prototype");
+	}
+}
